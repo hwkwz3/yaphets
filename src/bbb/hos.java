a5cb2e896df32dd1916535fc1c1aa0c4d3c8b351
Administrator <Administrator@USER-20160812KB>
2017-12-05 15:36:59 +0800

    test

---
+++ src/bbb/hos.java
@@ -0,0 +1,21 @@
+package bbb;
+
+public class hos {
+
+	 public static void main(String[] args) {
+		  
+	        int moneyEachDay = 0;
+	        int day = 10;
+	        int sum=0;
+	        for (int i = 1; i <= day; i++) {
+	            if(0==moneyEachDay)
+	                moneyEachDay = 1;
+	            else
+	                moneyEachDay *= 2;
+	              
+	            sum+=moneyEachDay;
+	              
+	            System.out.println(i + " 天之后，总数是: " + sum );
+	        }
+	    }
+}
